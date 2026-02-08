da0ef6e2d2842add269ed57c861e5f7b1c32d0af
NavneetKanna <navneetkanna04@gmail.com>
2026-02-08 18:59:46 +0530

    starting with structs and lifetime annotation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+struct ModelDimensions {
+    n_mels: i32,
+    n_audio_ctx: i32,
+    n_audio_state: i32,
+    n_audio_head: i32,
+    n_audio_layer: i32,
+    n_vocab: i32,
+    n_text_ctx: i32,
+    n_text_state: i32,
+    n_text_head: i32,
+    n_text_layer: i32
+}
+
+struct AudioEncoder {
+    n_mels: i32,
+    n_ctx: i32,
+    n_state: i32,
+    n_head: i32,
+    n_layer: i32
+}
+
+struct TextDecoder{
+    n_vocab: i32,
+    n_ctx: i32,
+    n_state: i32,
+    n_head: i32,
+    n_layer: i32
+}
+
+
+struct Whisper<'a> {
+    dims: &'a ModelDimensions,
+    encoder: &'a AudioEncoder,
+    decoder: &'a TextDecoder
+}
+
+
 fn main() {
-    println!("Hello, world!");
 }
